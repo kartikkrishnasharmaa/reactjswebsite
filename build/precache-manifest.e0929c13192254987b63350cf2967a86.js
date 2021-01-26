4092b9afc79d54320e0084e36566a1371c616682
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2021-01-26 13:09:30 +0530

    initial committ

---
+++ build/precache-manifest.e0929c13192254987b63350cf2967a86.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17290dd1dee9d858c7697923c64f68cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "2a804b11a59b19f0bcac",
+    "url": "/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a804b11a59b19f0bcac",
+    "url": "/static/js/main.f1db42b5.chunk.js"
+  },
+  {
+    "revision": "11495d3696929aa366fa",
+    "url": "/static/js/runtime-main.5bf8ac39.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/static/media/software.43049cae.jpeg"
+  }
+]);
